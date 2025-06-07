4643f7918f833cad406020588f1b5a0b7f7f1234
Joao <joaofegar@gmail.com>
2025-06-06 22:09:05 -0300

    Atualizar

--- Arvore/Arvore.java
+++ Arvore/Arvore.java
@@ -1,3 +1,207 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+
 public class Arvore {
+
+
     No raiz;
+
+
+    public int contarNos(No node) {
+        if (node == null) return 0;
+        return 1 + contarNos(node.esquerda) + contarNos(node.direita);
+    }
+
+
+    public void buscarPreOrdem(No node) {
+        if (node != null) {
+            System.out.println(node.valor + " ");
+            buscarPreOrdem(node.esquerda);
+            buscarPreOrdem(node.direita);
+        }
+    }
+
+
+    public void buscarEmOrdem(No node) {
+        if (node != null) {
+            buscarEmOrdem(node.esquerda);
+            System.out.println(node.valor + " ");
+            buscarEmOrdem(node.direita);
+        }
+    }
+
+
+    public void buscarPosOrdem(No node) {
+        if (node != null) {
+            buscarPosOrdem(node.esquerda);
+            buscarPosOrdem(node.direita);
+            System.out.println(node.valor + " ");
+        }
+    }
+
+
+    public void buscaEmNivel(No node) {
+        if (node != null) return;
+
+
+        Queue<No> fila = new LinkedList<>();
+        fila.add(node);
+
+
+        while (!fila.isEmpty()) {
+            No atual = fila.poll();
+            System.out.println(atual.valor + "  ");
+
+
+            if (atual.esquerda != null) {
+                fila.add(atual.esquerda);
+            }
+            if (atual.direita != null) {
+                fila.add(atual.direita);
+            }
+        }
+    }
+
+
+    public void preOrdemInterativo(No raiz) {
+        if (raiz == null) return;
+
+
+        Stack<No> pilha = new Stack<>();
+        pilha.push(raiz);
+
+
+        while (!pilha.isEmpty()) {
+            No atual = pilha.pop();
+            System.out.println(atual.valor + " ");
+
+
+            if (atual.direita != null) pilha.push(atual.direita);
+            if (atual.esquerda != null) pilha.push(atual.esquerda);
+        }
+    }
+
+
+    public void emOrdemInterativo(No raiz){
+        Stack<No> pilha = new Stack<>();
+        No atual = raiz;
+
+
+        while (atual != null || !pilha.isEmpty()){
+            while (atual != null){
+                pilha.push(atual);
+                atual = atual.esquerda;
+            }
+
+
+            atual = pilha.pop();
+            System.out.println(atual.valor);
+
+
+            atual = atual.direita;
+        }
+    }
+
+
+    public void posOrdemInterativo(No raiz) {
+        if (raiz == null) return;
+
+
+        Stack<No> pilha1 = new Stack<>();
+        Stack<No> pilha2 = new Stack<>();
+
+
+        pilha1.push(raiz);
+
+
+        while (!pilha1.isEmpty()) {
+            No atual = pilha1.pop();
+            pilha2.push(atual);
+
+
+            if (atual.esquerda != null) {
+                pilha1.push(atual.esquerda);
+            }
+
+
+            if (atual.direita != null) {
+                pilha1.push(atual.direita);
+            }
+        }
+
+
+        while (!pilha2.isEmpty()) {
+            No atual = pilha2.pop();
+            System.out.println(atual.valor);
+        }
+    }
+
+
+    public void emNivelInterativo(No node) {
+        if (node == null) return;
+
+
+        Queue<No> fila = new LinkedList<>();
+        fila.add(node);
+
+
+        while (!fila.isEmpty()) {
+            No atual = fila.poll();
+            System.out.println(atual.valor + "  ");
+
+
+            if (atual.esquerda != null) {
+                fila.add(atual.esquerda);
+            }
+            if (atual.direita != null) {
+                fila.add(atual.direita);
+            }
+        }
+    }
+
+
+    public int contarNosFolha(No node) {
+        if (node == null) {
+            return 0;
+        }
+
+
+        if (node.esquerda == null && node.direita == null) {
+            return 1;
+        }
+
+
+        return contarNosFolha(node.esquerda) + contarNosFolha(node.direita);
+    }
+
+    public int contarNosFolhas(No no){
+        if (no == null){
+            return 0;
+        }
+
+        if  (no.esquerda == null && no.direita == null){
+            return 1;
+        }
+
+        return contarNosFolhas(no.esquerda) + contarNosFolhas(no.direita);
+    }
+
+    int contarFolhas(No raiz) {
+        if (raiz == null) return 0;
+        Queue<No> fila = new LinkedList<>();
+        fila.add(raiz);
+        int folhas = 0;
+        while (!fila.isEmpty()){
+            No atual = fila.poll();
+            if (atual.esquerda == null && atual.direita == null) {
+                folhas++;
+            }
+            if (atual.esquerda != null) fila.add(atual.esquerda);
+            if (atual.direita != null) fila.add(atual.direita);
+        }
+        return folhas;
+    }
+
 }
--- Arvore/No.java
+++ Arvore/No.java
@@ -1,9 +1,18 @@
 public class No {
-        String valor;
-        No esquerda, direita;
+    String valor;
+    No esquerda, direita;
 
-        public No(String valor) {
-            this.valor = valor;
-            esquerda = direita = null;
+    public No(String valor) {
+        this.valor = valor;
+        esquerda = direita = null;
+    }
+    class No {
+        int chave, altura;
+        No esquerdo, direito;
+
+        No(int valor){
+            chave = valor;
+            altura = 1;
         }
+    }
 }
